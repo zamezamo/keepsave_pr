da750c3897cfd0fdaa15c98cd62feb9a7a2d7606
zamezamo <zamzame53@gmail.com>
2023-11-16 16:50:53 +0300

    fixed

--- app/src/main/java/com/zamezamo/keepsave/utils/DateTimeConverter.kt
+++ app/src/main/java/com/zamezamo/keepsave/utils/DateTimeConverter.kt
@@ -15,19 +15,19 @@ object DateTimeConverter {
 
         val ideaCalendar =
             GregorianCalendar(
-                dateAndTime.year,
-                dateAndTime.month,
-                dateAndTime.day,
-                dateAndTime.hour,
-                dateAndTime.minute
+                dateAndTime.year!!,
+                dateAndTime.month!!,
+                dateAndTime.day!!,
+                dateAndTime.hour!!,
+                dateAndTime.minute!!
             )
 
         return if (ideaCalendar.get(Calendar.YEAR) != currentCalendar.get(Calendar.YEAR))
             SimpleDateFormat("MMMM d, y, h:mm a").format(ideaCalendar.time)
         else if (ideaCalendar.get(Calendar.DAY_OF_YEAR) == currentCalendar.get(Calendar.DAY_OF_YEAR))
-            SimpleDateFormat("h:mm a").format(ideaCalendar.time)
+            "Today, " + SimpleDateFormat("h:mm a").format(ideaCalendar.time)
         else if (ideaCalendar.get(Calendar.DAY_OF_YEAR) - currentCalendar.get(Calendar.DAY_OF_YEAR) == 1)
-            "Tomorrow, " + SimpleDateFormat("Tomorrow, h:mm a").format(ideaCalendar.time)
+            "Tomorrow, " + SimpleDateFormat("h:mm a").format(ideaCalendar.time)
         else if (currentCalendar.get(Calendar.DAY_OF_YEAR) - ideaCalendar.get(Calendar.DAY_OF_YEAR) == 1)
             "Yesterday, " + SimpleDateFormat("h:mm a").format(ideaCalendar.time)
         else if (ideaCalendar.get(Calendar.WEEK_OF_YEAR) == currentCalendar.get(Calendar.WEEK_OF_YEAR))
